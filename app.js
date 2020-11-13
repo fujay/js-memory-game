263df6fa0f15db3748284acb96152444b45b3139
Marcel Klamm <fujitsustar@hotmail.de>
2020-11-13 03:23:24 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // card options
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+    ]
+})
